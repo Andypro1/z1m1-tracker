e193eced8eaacb925db76da4bf3a2fd06c8d1680
Andypro1 <871829+Andypro1@users.noreply.github.com>
2022-09-06 21:01:45 -0400

    Forgot to change svelte.config.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 //import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js';
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
